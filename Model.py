e11b63ffe51c80ed4c893bd4c08cc583c9bde200
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2021-10-17 13:47:16 +0200

    Update Model.py

--- Model.py
+++ Model.py
@@ -7,23 +7,33 @@ class Super_ress_model(torch.nn.Module):
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        self.layers1 = [
+        self.layers_1 = [
             self.conv_layer(input_shape[0], 32, 7,1,3),
             self.conv_layer(32, 64, 3,1,1),
             self.conv_layer(64, 128, 3,1,1),
         ]
         self.upsample = torch.nn.Upsample(size=None, scale_factor=3, mode='nearest', align_corners=None)
 
-        self.layers2 = [
+        self.layers_2 = [
             self.conv_layer(128, 128, 3,1,1),
-            self.conv_layer(128,128, 3,1,1),
-            self.conv_layer(128, 3, 3,1,1),
+            self.conv_layer(128,64, 3,1,1),
+            self.conv_layer(64, 3, 3,1,1),
         ]
 
-        self.model_layers1 = nn.Sequential(*self.layers1)
+        for i in range(len(self.layers_2)):
+            if hasattr(self.layers_2[i], "weight"):
+                torch.nn.init.xavier_uniform_(self.layers_2[i].weight)
+                torch.nn.init.zeros_(self.layers_2[i].bias)
+
+        for i in range(len(self.layers_1)):
+            if hasattr(self.layers_1[i], "weight"):
+                torch.nn.init.xavier_uniform_(self.layers_1[i].weight)
+                torch.nn.init.zeros_(self.layers_1[i].bias)
+
+        self.model_layers1 = nn.Sequential(*self.layers_1)
         self.model_layers1.to(self.device)
 
-        self.model_layers2 = nn.Sequential(*self.layers2)
+        self.model_layers2 = nn.Sequential(*self.layers_2)
         self.model_layers2.to(self.device)
 
         print(self.model_layers2)
@@ -31,11 +41,12 @@ class Super_ress_model(torch.nn.Module):
 
 
     def forward(self,x):
+        x_bypass = self.upsample(x)
         layers_1 = self.model_layers1(x)
         upsample = self.upsample(layers_1)
         layers2 = self.model_layers2(upsample)
 
-        return layers2
+        return layers2 + x_bypass
 
 
     def conv_layer(self, inputs, outputs,kernel, stride,padding):
@@ -43,6 +54,3 @@ class Super_ress_model(torch.nn.Module):
             nn.Conv2d(inputs, outputs, kernel_size=kernel, stride=stride, padding=padding),
             nn.BatchNorm2d(outputs),
             nn.ReLU(inplace=True))
-
-
-
