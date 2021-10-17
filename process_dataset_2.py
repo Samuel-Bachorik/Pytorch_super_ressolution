bf3f68b63cbbc1faf91a67c09dbf94d6fbcd99b6
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2021-10-17 15:00:45 +0200

    Update process_dataset_2.py

--- process_dataset_2.py
+++ process_dataset_2.py
@@ -18,16 +18,16 @@ class Process_dataset:
         self.aug_count = aug_count
         self.training_images = []
         self.training_labels = []
-        self.path = ("C:/Users/Samuel/PycharmProjects/Super_ressolution/dataset2")
+        self.path = path
         Loader = ImagesLoader(self.path, 128, 128)
         images, labels = Loader.load_images()
 
         self.training_images.append(images)
         self.training_labels.append(labels)
-        print("HOVNO")
+
 
     def get_training_batch(self,batch_size):
-        return self.get_batch(self.training_images,self.training_labels,32)
+        return self.get_batch(self.training_images,self.training_labels,batch_size)
 
 
     def get_batch(self,images,labels,batch_size):
@@ -51,12 +51,11 @@ class Process_dataset:
         group_idx = numpy.random.randint(len(self.training_images))
         image_idx = numpy.random.randint(len(self.training_images[group_idx]))
 
-        image_np = numpy.array(images[group_idx][image_idx])
-        label_np = numpy.array(labels[group_idx][image_idx])
-        #print(numpy.min(image_np), numpy.max(image_np ),"KOKOT",numpy.min(label_np), numpy.max(label_np ))
-        # if self._rnd(0, 1) > 0.1:
+        image_np = numpy.array(images[group_idx][image_idx])/255.0
+        label_np = numpy.array(labels[group_idx][image_idx])/255.0
+
         image_np, mask_np = self._augmentation_flip(image_np, label_np)
-        #image_np, mask_np = self._augmentation_noise(image_np, mask_np)
+        image_np, mask_np = self._augmentation_noise(image_np, mask_np)
 
         result_x = torch.from_numpy(image_np)
         result_y = torch.from_numpy(mask_np)
@@ -76,22 +75,21 @@ class Process_dataset:
     def _augmentation_noise(self, image_np,label_np):
         brightness = self._rnd(-0.25, 0.25)
         contrast = self._rnd(0.5, 1.5)
-        noise = 0.05 * (2.0 * numpy.random.rand(3, 384, 384) - 1.0)
+        #noise = 0.05 * (2.0 * numpy.random.rand(3, 384, 384) - 1.0)
 
-        noise_low = numpy.swapaxes(noise, 0, 2)
-        noise_low = cv2.resize(noise_low, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
-        noise_low = numpy.swapaxes(noise_low, 0, 2)
+        #noise_low = numpy.swapaxes(noise, 0, 2)
+        #noise_low = cv2.resize(noise_low, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
+        #noise_low = numpy.swapaxes(noise_low, 0, 2)
 
         img_result = image_np + brightness
         img_result = 0.5 + contrast * (img_result - 0.5)
-        img_result = img_result + noise_low
+        #img_result = img_result + noise_low
 
         label_result = label_np + brightness
         label_result = 0.5 + contrast * (label_result - 0.5)
-        label_result = label_result + noise
+        #label_result = label_result + noise
 
         return numpy.clip(img_result, 0.0, 1.0),numpy.clip(label_result, 0.0, 1.0)
-        #return img_result,label_result
 
 
     def _rnd(self, min_value, max_value):
