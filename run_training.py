c57a4ee7ef6ab05d5095b60a9c8fbcf8b74dd957
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2021-10-17 13:16:00 +0200

    Update run_training.py

--- run_training.py
+++ run_training.py
@@ -7,7 +7,7 @@ import os
 import time
 from datetime import datetime
 
-path =  ("C:/Users/Samuel/PycharmProjects/Super_ressolution/dataset2")
+path =  ("C:/Users/samue/PycharmProjects/reinforcement_learning_env/dataset2")
 loader = Process_dataset(path,128,128,3)
 
 model = Super_ress_model()
@@ -17,13 +17,11 @@ model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
 for i in range(100):
-    x, y = loader.get_training_batch(32)
+    x, y = loader.get_training_batch(4)
     x = x.to(model.device)
     y = y.to(model.device)
     y_pred= model.forward(x)
 
-    print(y.shape)
-    print(y_pred.shape)
     loss = ((y-y_pred)**2).mean()
     print(loss)
     optimizer.zero_grad()
@@ -39,4 +37,3 @@ im = numpy.moveaxis(im, 0, 2)
 img = (im*255).astype(numpy.uint8)
 imgg = Image.fromarray(img)
 imgg.show()"""
-
