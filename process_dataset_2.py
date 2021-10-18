c4da477d548e6146e94c57afcedcfa6560e572d9
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2021-10-18 18:24:58 +0200

    Update process_dataset_2.py

--- process_dataset_2.py
+++ process_dataset_2.py
@@ -3,12 +3,12 @@ import PIL
 from PIL import Image, ImageOps
 import numpy
 import math
+import sys
 import os
 import cv2
 from main import ImagesLoader
 import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
-
 
 class Process_dataset:
     def __init__(self,path, height,width,aug_count):
@@ -25,9 +25,57 @@ class Process_dataset:
         self.training_images.append(images)
         self.training_labels.append(labels)
 
+        images_aug,labels_aug = self._auqumentation(self.training_images,self.training_labels)
+
+        self.training_images.append(images_aug)
+        self.training_labels.append(labels_aug)
+
 
     def get_training_batch(self,batch_size):
         return self.get_batch(self.training_images,self.training_labels,batch_size)
+
+
+    def process_augumentation(self,image,label):
+        image_aug = numpy.array((image), dtype=numpy.uint8)
+        label_aug = numpy.array((label), dtype=numpy.uint8)
+        angle_max = 25
+        angle = self._rnd(-angle_max, angle_max)
+
+        image_in = Image.fromarray(numpy.moveaxis(image_aug, 0, 2), 'RGB')
+        mask_in = Image.fromarray(numpy.moveaxis(label_aug, 0, 2), 'RGB')
+
+        image_aug = image_in.rotate(angle,resample=Image.BICUBIC)
+        mask_aug = mask_in.rotate(angle,resample=Image.BICUBIC)
+
+        image_aug = numpy.array(image_aug)
+        mask_aug = numpy.array(mask_aug)
+
+        image_aug = numpy.swapaxes(image_aug, 0, 2)
+        mask_aug = numpy.swapaxes(mask_aug, 0, 2)
+
+        return image_aug, mask_aug
+
+
+    def _auqumentation(self,images,labels):
+        count = len(images[0])
+        counter= 0
+        images_aug = numpy.zeros((count * 2, 3, 128, 128), dtype=numpy.uint8)
+        labels_aug = numpy.zeros((count * 2, 3, 384, 384), dtype=numpy.uint8)
+
+        with ThreadPoolExecutor(max_workers=count * 2) as executor:
+            results = [None] * count * 2
+            for x in range(count * 2):
+                results[x] = executor.submit(self.process_augumentation,images[0][counter],labels[0][counter])
+                counter+=1
+                if counter == count:
+                    counter = 0
+
+            counter = 0
+            for f in concurrent.futures.as_completed(results):
+                images_aug[counter], labels_aug[counter] = f.result()[0], f.result()[1]
+                counter += 1
+
+        return images_aug, labels_aug
 
 
     def get_batch(self,images,labels,batch_size):
@@ -57,8 +105,8 @@ class Process_dataset:
         image_np, mask_np = self._augmentation_flip(image_np, label_np)
         image_np, mask_np = self._augmentation_noise(image_np, mask_np)
 
-        result_x = torch.from_numpy(image_np)
-        result_y = torch.from_numpy(mask_np)
+        result_x = torch.from_numpy(image_np).float()
+        result_y = torch.from_numpy(mask_np).float()
 
         return result_x, result_y
 
