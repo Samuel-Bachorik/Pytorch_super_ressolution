8dbaabf7b8b33fb726db479fd684db3b15a17b86
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2021-10-19 18:46:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,17 @@ import math
 import os
 
 class ImagesLoader:
-    def __init__(self, path,height,width):
+    def __init__(self, path,in_ress,out_ress):
         self.path   = path
-        self.height = height
-        self.width  = width
+        self.in_ress = in_ress
+        self.out_ress  = out_ress
         self.channels = 3
 
         f = os.listdir(path)
         self.dataset_count = len(f)
-        self.images = numpy.zeros((self.dataset_count, self.channels, self.height, self.width), dtype=numpy.uint8)
-        self.labels = numpy.zeros((self.dataset_count, self.channels, 384, 384), dtype=numpy.uint8)
+        self.images = numpy.zeros((self.dataset_count, self.channels, self.in_ress, self.in_ress), dtype=numpy.uint8)
+        self.labels = numpy.zeros((self.dataset_count, self.channels, self.out_ress, self.out_ress), dtype=numpy.uint8)
 
-
-        #self.load_images()
 
     def load_images(self):
         f = os.listdir(self.path)
@@ -28,23 +26,22 @@ class ImagesLoader:
             #x = ImageOps.exif_transpose(x)
 
             if y.size[0] > y.size[1]:
-                new_size = (y.size[0] / y.size[1]) * 384
-                y = y.resize((math.floor(new_size), 384))
+                new_size = (y.size[0] / y.size[1]) * self.out_ress
+                y = y.resize((math.floor(new_size), self.out_ress))
             else:
-                new_size = (y.size[1] / y.size[0]) * 384
-                y = y.resize((384, math.floor(new_size)))
+                new_size = (y.size[1] / y.size[0]) * self.out_ress
+                y = y.resize((self.out_ress, math.floor(new_size)))
 
-            y = y.crop((math.floor((y.size[0] - 384) / 2), 0, math.floor((y.size[0] - 384) / 2) + 384, 384))
+            y = y.crop((math.floor((y.size[0] - self.out_ress) / 2), 0, math.floor((y.size[0] - self.out_ress) / 2) + self.out_ress, self.out_ress))
 
             y_np = numpy.array(y)
             y_np = numpy.moveaxis(y_np,-1,0)
             self.labels[i] = y_np
 
-            x = y.resize((128,128))
-            #x.show()
+            x = y.resize((self.in_ress,self.in_ress))
+
             x_np = numpy.array(x)
             x_np = numpy.moveaxis(x_np,-1,0)
-
 
             self.images[i] = x_np
 
