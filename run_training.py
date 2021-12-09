a7cbb25312e93a28c0e3803544cd20e71cc41c7f
Samuel Bachorík <61843287+Samuel-Bachorik@users.noreply.github.com>
2021-12-09 12:04:13 +0100

    Update run_training.py

--- run_training.py
+++ run_training.py
@@ -1,16 +1,15 @@
 from process_dataset_2 import Process_dataset
-from Model import Super_ress_model
-import numpy
-from PIL import Image
+from residual_model import Super_ress_model
 import torch
 import os
 import time
 from datetime import datetime
-import math
 import matplotlib.pyplot as plt
+from sobel_operator_model import Sobel_operator
+from graphing_class import CreateGraph
 if __name__ == '__main__':
-    path =  ("C:/Users/Samuel/PycharmProjects/Super_ressolution/dataset2")
-    loader = Process_dataset(path, in_ress=128, out_ress=512, aug_count=3)
+    path =  ("C:/Users/Samuel/PycharmProjects/Super_ressolution/dataset3")
+    loader = Process_dataset(path, in_ress=64, out_ress=512, aug_count=3)
 
     model = Super_ress_model()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -22,7 +21,7 @@ if __name__ == '__main__':
     print(time.time())
 
     epoch_count = 200
-    batch_size = 16
+    batch_size = 3
     batch_count = (loader.get_training_count() + batch_size) // batch_size
     print(batch_count, "Batch count")
     maxloss = 99999.0
@@ -40,9 +39,6 @@ if __name__ == '__main__':
             y = y.to(model.device)
 
             y_pred= model.forward(x)
-
-            SNR = math.log10((abs(y) / (abs(y - y_pred) + 0.00001)).mean())
-            print(SNR,"SNR")
 
 
             loss = ((y - y_pred) ** 2).mean()
